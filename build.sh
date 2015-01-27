b229d0596c437e953742ba738926c9680316aba8
TJ Krusinski <tjkrus@gmail.com>
2015-01-27 09:47:12 -0500

    move over to metalsmith

--- build.sh
+++ build.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
 
-# `marked` is an npm package
-# npm install marked -g
+# Welcome to the orlando devs build script
+# I don't write much bash so bear with me
 
-cat readme.md | marked > index.html
+# install npm deps if need be
+# default assumes that the deps are installed
+
+checkInstall() {
+  if [ -e node_modules/.bin/metalsmith ]; then
+    build;
+  else
+    installDeps;
+  fi
+}
+
+# Do the build
+
+build() {
+  ./node_modules/.bin/metalsmith
+}
+
+# ask the user to install the npm deps if the terminal is a tty
+# else just bail
+
+installDeps() {
+  # -t being 1 means stdout is a tty
+  if [ -t 1 ]; then
+    
+    echo "looks like you need to npm install"
+    echo "would you like us to do that for you? (y,n)"
+
+    read doinstall
+
+    # do pattern matching cause people may use 'yes' or 'y'
+    if [[ $doinstall == y* ]]; then
+      printf "great, installing npm deps for you, this may take a moment\n"
+      npm install
+
+      build;
+      exit 0;
+    else
+      echo "bailing since you chose not to install the deps"
+      exit 0;
+    fi
+     
+  else
+    echo "you are not a tty, peace"
+    exit 1;
+  fi
+}
+
+# start things off
+checkInstall
---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var http = require('http');
+var st = require('st');
+
+var port = process.env.PORT || 3000;
+var stOpts = {
+  // this is where metalsmith dumps the files
+  path: 'build',
+  // tell st to use `build/index.html` for `GET /`
+  index: './index.html',
+  gzip: true
+};
+
+/**
+ *  A wee little server, just right for our needs
+ */
+
+http.createServer(st(stOpts)).listen(port, function() {
+  console.log('Orlando developers server listening on port %s', port);
+});
